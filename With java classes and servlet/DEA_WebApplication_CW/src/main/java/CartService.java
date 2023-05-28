ba7602d8748c14f23e40f46b8f11464591d1e0fd
Umindu Nimsara <nimsara.umi@gmail.com>
2023-05-28 22:16:16 +0530

    With java classes and servlet

---
+++ With java classes and servlet/DEA_WebApplication_CW/src/main/java/CartService.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class CartService {
+  private List<OrderItem> cartItems = new ArrayList<>();
+
+  public List<OrderItem> getCartItems() {
+    return cartItems;
+  }
+
+  public void addToCart(OrderItem item) {
+    cartItems.add(item);
+  }
+
+  public void removeFromCart(OrderItem item) {
+    cartItems.remove(item);
+  }
+}
---
+++ With java classes and servlet/DEA_WebApplication_CW/src/main/java/CartServlet.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class CartServlet extends HttpServlet {
+
+private static final long serialVersionUID = 1L;
+private CartService cartService;
+
+@Override
+public void init() throws ServletException {
+    cartService = new CartService();
+  }
+
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	    String action = request.getParameter("action");
+	    if (action != null && action.equals("removeFromCart")) {
+	        // Retrieve the title parameter and remove the item from the cart
+	        String title = request.getParameter("title");
+	        if (title != null && !title.isEmpty()) {
+	            HttpSession session = request.getSession();
+	            List<OrderItem> cartItems = (List<OrderItem>) session.getAttribute("cartItems");
+	            if (cartItems != null) {
+	                // Find the item with the matching title and remove it from the cart
+	                for (OrderItem item : cartItems) {
+	                    if (item.getTitle().equals(title)) {
+	                        cartItems.remove(item);
+	                        break;
+	                    }
+	                }
+	            }
+	        }
+  }
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // Pass the CartService instance to the JSP file as an attribute
+    request.setAttribute("cartService", cartService);
+    request.getRequestDispatcher("cart.jsp").forward(request, response);
+  }
+}
